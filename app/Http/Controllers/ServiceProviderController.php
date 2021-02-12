4c8a97457b85d58044fc6a6dcdfe485539c0be53
TheOnlySmartBoy <o2jose43@gmail.com>
2021-02-12 13:52:34 +0300

    Added service provider index logic

--- app/Http/Controllers/ServiceProviderController.php
+++ app/Http/Controllers/ServiceProviderController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ServiceProvider;
 
 class ServiceProviderController extends Controller
 {
-    //
+    public function index(){
+        $provider = ServiceProvider::get();
+        return view('serviceprovider.index',compact('provider'));
+    }
 }
