044568d3544a74d79f8bf559405a6e52779b2cba
TheOnlySmartBoy <o2jose43@gmail.com>
2021-02-12 14:05:31 +0300

    Updated service provider retriever logic

--- app/Http/Controllers/ServiceProviderController.php
+++ app/Http/Controllers/ServiceProviderController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ServiceProvider;
+use DB;
 
 class ServiceProviderController extends Controller
 {
     public function index(){
-        $provider = ServiceProvider::get();
-        return view('serviceprovider.index',compact('provider'));
+        $service_providers = DB::table('service_providers')
+        ->join('wards', 'service_providers.ward', 'wards.id')
+        ->join('categories', 'service_providers.category', 'categories.id')
+        ->select('service_providers.*', 'wards.name As ward_name', 'categories.name As category_name')
+        ->get();
+        return view('serviceprovider.index',compact('service_providers'));
     }
 }
