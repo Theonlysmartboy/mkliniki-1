d1127f18e3bc97cbad539430e24530adb7dde147
netubuli <netubuli@gmail.com>
2021-02-12 15:10:22 +0300

    service provider view updated

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="wrapper">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="span3">
                 <div class="sidebar">
