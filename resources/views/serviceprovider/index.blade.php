3d4022beaacdde22ed0cdf58b118645cc93a29ff
TheOnlySmartBoy <o2jose43@gmail.com>
2021-02-17 12:47:42 +0300

    added the add provider button

--- resources/views/serviceprovider/index.blade.php
+++ resources/views/serviceprovider/index.blade.php
@@ -7,12 +7,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>DataTables</h1>
+            <h1>Services</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">DataTables</li>
+              <li class="breadcrumb-item active">Services</li>
             </ol>
           </div>
         </div>
@@ -26,7 +26,7 @@
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Services</h3>
+                <h3 class="card-title btn btn-sm btn-success"><i class="fa fa-plus" id="addprovider"></i></h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
